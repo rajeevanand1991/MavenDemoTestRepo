d26fb5610dfd7db5334b8b83461e88011b3ffb25
rajeevanand1991 <rajeevanand1991@gmail.com>
2022-10-26 16:30:16 +0530

    To know about maven

---
+++ MavenDemoTest/src/test/java/com/qa/tests/DemoTest2.java
@@ -0,0 +1,39 @@
+package com.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+	
+	@Test
+	public void sum() {
+		System.out.println("SUM method:");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(30, a+b);
+	}
+	
+	@Test
+	public void sub() {
+		System.out.println("SUB method:");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(10, b-a);
+	}
+	
+	@Test
+	public void div() {
+		System.out.println("DIV method:");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(2, b/a);
+	}
+	
+	@Test
+	public void multiply() {
+		System.out.println("MULTIPLY method:");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(200, a*b);
+	}
+}
